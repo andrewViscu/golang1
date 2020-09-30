0d45006b0c5be25cecfae15cc164a94f90f3299d
AndrewViscu <samsungmvideo33@gmail.com>
2020-10-01 00:17:52 +0300

    fixed errors

--- back-end/db.go
+++ back-end/db.go
@@ -1,28 +1,28 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"time"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"time"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
 
-var connectionURI = "mongodb+srv://andrewViscu:(password)@cluster0.kljzg.mongodb.net/foo?retryWrites=true&w=majority"
+// var connectionURI = "mongodb+srv://andrewViscu:AquaDash1324@cluster0.kljzg.mongodb.net/foo?retryWrites=true&w=majority"
 
-func db() *mongo.Client {
-	clientOptions := options.Client().ApplyURI(connectionURI)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to MongoDB!")
-	// defer client.Disconnect(ctx)
-	return client
-}
+// func db() *mongo.Client {
+// 	clientOptions := options.Client().ApplyURI(connectionURI)
+// 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+// 	client, err := mongo.Connect(ctx, clientOptions)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("Connected to MongoDB!")
+// 	// defer client.Disconnect(ctx)
+// 	return client
+// }
