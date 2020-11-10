b64c101860c776a1bd5ca3d507bfa27577363743
Viscu Andrew <samsungmvideo33@gmail.com>
2020-11-10 15:19:56 +0200

    quick fix of quick fix

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Connect() *mongo.Client {
-	var dbPass, dbUser string
-	dbUser, dbPass = GetCredentials()
-	clientURI := "mongodb+srv://" + dbUser + ":" + dbPass + "@cluster0.kljzg.mongodb.net/foo?retryWrites=true&w=majority"
+	clientURI := "mongodb+srv://" + os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@cluster0.kljzg.mongodb.net/foo?retryWrites=true&w=majority"
 	clientOptions := options.Client().ApplyURI(clientURI)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
